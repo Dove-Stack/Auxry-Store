4ee800041265d2f5b63d74c196c455b655a54156
Dove Stack <flawal664@gmail.com>
2025-08-21 01:01:21 +0100

    commit & push

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,13 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
 import { ToastContainer } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css";
+// import "react-toastify/dist/ReactToastify.css";
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <StoreContextProvider>
+      <Toaster position='top-right' reverseOrder={false} />
       <App />
       <ToastContainer/>
     </StoreContextProvider> 
