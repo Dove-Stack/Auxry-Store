a3af0d8e0c1505c569b17589a01bad9ec831724d
Dove Stack <flawal664@gmail.com>
2025-08-26 10:10:47 +0100

    style: update Footer links to correct navigation paths and add Newsletter component

---
+++ frontend/src/components/Newsletter/Newsletter.jsx
@@ -0,0 +1,19 @@
+import './Newsletter.css'
+
+const Newsletter = () => {
+  return (
+    <div className='newsletter'>
+        <div className="sub-newsletter">
+            <h2>Recieve info on new Items, Deals, Sales and more....</h2>
+            <span>We will email you a voucher worth of $30 off your first order over $180</span>
+            <div className='newsletter-form'>
+                <input type="email" name="" id="" /><button>Suscribe</button><br />
+                <span>By suscribing you agree to our Terms & Conditions and Privacy & Cookies Policy</span>
+            </div>
+        </div>
+        <div className="sub-newsletter"></div>
+    </div>
+  )
+}
+
+export default Newsletter
