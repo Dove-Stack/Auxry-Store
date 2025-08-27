49c50a3f082f40c530bb40ebc02cdb7a09870c73
Dove Stack <flawal664@gmail.com>
2025-08-27 19:17:30 +0100

    updated Logic adding order number and date to order model and product orders page. and also replaced `key={index}` with `key={order._id}` in ProductOrders component for better key management in React.

--- frontend/src/pages/ProductOrders/ProductOrders.jsx
+++ frontend/src/pages/ProductOrders/ProductOrders.jsx
@@ -14,22 +14,39 @@ const ProductOrders = () => {
       { headers: { token } }
     );
     setData(response.data.data);
-    console.log(response.data.data)
+    console.log(response.data.data);
   };
 
   useEffect(() => {
-    if(token) {
-        fetchOrders();
+    if (token) {
+      fetchOrders();
     }
-  }, [token])
+  }, [token]);
 
-  return <div>
-    <h1>
-        {data.map((order, index ) => {
-            
-        })}
-    </h1>
-  </div>;
+  return (
+    <div className="product-orders">
+      <h1>Your Orders</h1>
+      {data.length === 0 ? (
+        <p>No order Found</p>
+      ) : (
+        <ul>
+          {data.map((order) => (
+            <li key={order._id}>
+              <p>Order Number: {order.orderNumber}</p>
+              <p>Amount: ${order.amount}</p>
+              <p>Status: {order.payment ? "Paid ✅" : "Pending 🔃"}</p>
+              {/* <p>Order ID: {order._id}</p>
+            <p>Date: {new Date(order.createdAt).toLocaleDateString()}</p>
+            <p>Total Amount: ${order.totalAmount}</p>
+            <p>Status: {order.status}</p>
+            <hr /> */}
+              {/* <p>Order ID: {}</p> */}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default ProductOrders;
