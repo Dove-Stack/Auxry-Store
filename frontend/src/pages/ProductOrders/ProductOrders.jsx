6192ae9278eeef0f0a4dad9e6d1e44a6670384c4
Dove Stack <flawal664@gmail.com>
2025-08-27 21:43:08 +0100

    last one 😉

--- frontend/src/pages/ProductOrders/ProductOrders.jsx
+++ frontend/src/pages/ProductOrders/ProductOrders.jsx
@@ -1,49 +1,49 @@
-// import React, { useContext, useEffect, useState } from "react";
-// import "./ProductOrders.css";
-// import { StoreContext } from "../../context/StoreContext";
-// import axios from "axios";
+/* import React, { useContext, useEffect, useState } from "react";
+import "./ProductOrders.css";
+import { StoreContext } from "../../context/StoreContext";
+import axios from "axios";
 
-// const ProductOrders = () => {
-//   const { url, token } = useContext(StoreContext);
-//   const [data, setData] = useState([]);
+const ProductOrders = () => {
+  const { url, token } = useContext(StoreContext);
+  const [data, setData] = useState([]);
 
-//   const fetchOrders = async () => {
-//     const response = await axios.post(
-//       url + "/api/order/userorders",
-//       {},
-//       { headers: { token } }
-//     );
-//     setData(response.data.data);
-//     console.log(response.data.data);
-//   };
+  const fetchOrders = async () => {
+    const response = await axios.post(
+      url + "/api/order/userorders",
+      {},
+      { headers: { token } }
+    );
+    setData(response.data.data);
+    console.log(response.data.data);
+  };
 
-//   useEffect(() => {
-//     if (token) {
-//       fetchOrders();
-//     }
-//   }, [token]);
+  useEffect(() => {
+    if (token) {
+      fetchOrders();
+    }
+  }, [token]);
 
-//   return (
-//     <div className="product-orders">
-//       <h1>Your Orders</h1>
-//       {data.length === 0 ? (
-//         <p>No order Found</p>
-//       ) : (
-//         <ul>
-//           {data.map((order) => (
-//             <li key={order._id}>
-//               <p>Order Number: {order.orderNumber}</p>
-//               <p>Amount: ${order.amount}</p>
-//               <p>Status: {order.payment ? "Paid ✅" : "Pending 🔃"}</p>
-//             </li>
-//           ))}
-//         </ul>
-//       )}
-//     </div>
-//   );
-// };
+  return (
+    <div className="product-orders">
+      <h1>Your Orders</h1>
+      {data.length === 0 ? (
+        <p>No order Found</p>
+      ) : (
+        <ul>
+          {data.map((order) => (
+            <li key={order._id}>
+              <p>Order Number: {order.orderNumber}</p>
+              <p>Amount: ${order.amount}</p>
+              <p>Status: {order.payment ? "Paid ✅" : "Pending 🔃"}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
-// export default ProductOrders;
+export default ProductOrders; */
 
 import React, { useContext, useEffect, useState } from "react";
 import "./ProductOrders.css";
