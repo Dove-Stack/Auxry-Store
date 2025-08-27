b1f5247e19344d2ad953ce5f2563c2fd3c4f010d
Dove Stack <flawal664@gmail.com>
2025-08-27 16:41:32 +0100

    Formatted code block

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -31,16 +31,15 @@ const removeFromCart = async (req, res) => {
     let userData = await userModel.findById(req.body.userId);
     let cartData = await userData.cartData;
 
-    if(cartData[req.body.itemId] > 0) {
+    if (cartData[req.body.itemId] > 0) {
       cartData[req.body.itemId] -= 1;
     }
 
-    await userModel.findByIdAndUpdate(req.body.userId,{cartData});
-    res.json({success: true, message: "Removed from Cart"})
-
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    res.json({ success: true, message: "Removed from Cart" });
   } catch (error) {
     console.log(error);
-    res.json({success: false, message: "Error"})
+    res.json({ success: false, message: "Error" });
   }
 };
 
@@ -50,11 +49,10 @@ const getCart = async (req, res) => {
   try {
     let userData = await userModel.findById(req.body.userId);
     let cartData = await userData.cartData;
-    res.json({success: true, cartData})
-
+    res.json({ success: true, cartData });
   } catch (error) {
     console.log(error);
-    res.json({success: false, message: "Error"})
+    res.json({ success: false, message: "Error" });
   }
 };
 
