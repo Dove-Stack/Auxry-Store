38409af65129a032459553a3f485f6b862c3d57e
Dove Stack <flawal664@gmail.com>
2025-08-27 21:35:51 +0100

    currently modifying: All logic

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  items: { type: Array, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  items: [{ name: String, salePrice: Number, quantity: Number }],
   amount: { type: Number, required: true },
   address: { type: Object, required: true },
-  status: { type: String, default: "Item Loading..." },
   payment: { type: Boolean, default: false },
+  status: { type: String, default: "Item Loading..." },
   orderNumber: { type: String, unique: true, required: true },
-  date: { type: Date, default: Date.now() },
+  date: { type: Date, default: Date.now },
 });
 
 const orderModel =
